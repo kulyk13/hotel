ae8b2c90ece0fe06c1f53b03c245ed6873aa6f4d
kulyk13 <13dimon1313@gmail.com>
2021-01-28 04:11:50 +0200

    correct-adaptive-576px

--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
+// Swiper slider
 const reviewSlider = new Swiper('.review-slider', {
-    lazy: true,
-    loop: true,
-    navigation: {
-        nextEl: '.slider-arrow-forward',
-        prevEl: '.slider-arrow-back',
-      },
-    // Responsive breakpoints
-    breakpoints: {
-      // window >=320px
-        320: {
-          spaceBetween: 30,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-          },
+  lazy: true,
+  loop: true,
+  navigation: {
+      nextEl: '.slider-arrow-forward',
+      prevEl: '.slider-arrow-back',
+    },
+  // Responsive breakpoints
+  breakpoints: {
+    // window >=320px
+      320: {
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
         },
       },
-    });
+    },
+  });
+
+// Accordion
+const accordions = document.querySelectorAll('.footer-top__list');
+
+for (item of accordions) {
+  item.addEventListener('click', function() {
+    if(this.classList.contains('active')) {
+      this.classList.remove('active');
+    } else {
+      for (el of accordions) {
+        el.classList.remove('active');
+      }
+      this.classList.add('active');
+    }
+  })
+}
